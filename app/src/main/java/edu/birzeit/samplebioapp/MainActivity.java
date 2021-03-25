8421f03ee6b9708427a75fe6f3c113ff7ce1ed48
Mohammad Ihraiz <mohammad.a.ihraiz@gmail.com>
2021-03-25 16:45:07 +0200

    Version 1.0

---
+++ app/src/main/java/edu/birzeit/samplebioapp/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.birzeit.samplebioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtHobbies;
+    private TextView txtHobbies;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupViews();
+    }
+
+    private void setupViews() {
+        edtHobbies = findViewById(R.id.edtHobbies);
+        txtHobbies = findViewById(R.id.txtHobbies);
+    }
+
+    public void btn_OnClick(View view) {
+        String hobbies = edtHobbies.getText().toString();
+        txtHobbies.setText(hobbies);
+        txtHobbies.setVisibility(View.VISIBLE);
+    }
+}
